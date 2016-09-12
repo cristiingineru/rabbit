ba640a067cdb02d3bc1397f46f5ba899cf26a345
Cristi Ingineru <cristi.ingineru@gmail.com>
2016-09-12 21:31:58 +0300

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -14,8 +14,6 @@ module.exports = function (config) {
         // list of files / patterns to load in the browser
         files: [
             'jquery.js',
-            'jquery.flot.js',
-            'jquery*.js',
             'spec/*.Test.js',
         ],
 
