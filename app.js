0f74c8a7d1d6631e78c385bb70044b47cfff324b
Cristi Ingineru <cristi.ingineru@gmail.com>
2016-09-13 23:08:32 +0300

    More tests

--- app.js
+++ app.js
@@ -3,18 +3,39 @@
 jQuery = $;
 
 function Eye(ctx) {
-  this.draw = function () {
+  this.draw = function (opt) {
+    var startAngle = 0,
+      endAngle = 2 * Math.PI;
+
+    if (opt && opt.side === 'left') {
+      startAngle = 3* Math.PI / 4;
+      endAngle = 5 * Math.PI / 4;
+    } else if (opt && opt.side === 'right') {
+      startAngle = 7 * Math.PI / 4;
+      endAngle = 1 * Math.PI / 4;
+    }
+
     ctx.beginPath();
-    ctx.arc(100, 75 , 50, 0, 2 * Math.PI);
+    ctx.arc(0, 0 , 10, startAngle, endAngle);
     ctx.stroke();
   }
 }
 
 function Face(ctx) {
+
   var eye1 = new Eye(ctx),
     eye2 = new Eye(ctx);
-  this.draw = function () {
-    eye1.draw();
-    eye2.draw();
+
+  this.draw = function (opt) {
+    if (opt && opt.side === 'left') {
+      eye1.draw(opt);
+    }
+    else if (opt && opt.side === 'right') {
+      eye2.draw(opt);
+    } else {
+      ctx.strokeRect(-10, -10, 20, 20);
+      eye1.draw(opt);
+      eye2.draw(opt);
+    }
   }
 }
--- spec/face.Test.js
+++ spec/face.Test.js
@@ -7,9 +7,9 @@ describe('Face', function () {
     var placeholder, ctx, face;
 
     beforeEach(function () {
-        var fixture = setFixtures('<div id="demo-container" style="width: 800px;height: 600px">').find('#demo-container').get(0);
+        var fixture = setFixtures('<div id="demo-container" style="width: 400px;height: 300px">').find('#demo-container').get(0);
 
-        placeholder = $('<canvas id="placeholder" style="width: 100%;height: 100%" />');
+        placeholder = $('<canvas id="placeholder"  />');
         placeholder.appendTo(fixture);
         ctx = placeholder[0].getContext('2d');
         face = new Face(ctx);
@@ -19,12 +19,45 @@ describe('Face', function () {
         $('#demo-container').empty();
     });
 
-    it('should have 2 eyes', function () {
+    it('should have 2 eyes of the same size', function () {
         face.draw();
 
         var eyes = circles(ctx.stack());
 
         expect(eyes.length).toBe(2);
+        expect(radius(eyes[0])).toBe(radius(eyes[1]));
+    });
+
+    it('should have the eyes aligned', function () {
+        face.draw();
+
+        var eyes = circles(ctx.stack());
+
+        expect(eyes.length).toBe(2);
+        expect(position(eyes[0])).toEqual(position(eyes[1]));
+    });
+
+    it('should contain the eyes inside it`s area', function () {
+        face.draw();
+
+        var eyes = circles(ctx.stack());
+        var area = rectangles(ctx.stack())[0];
+
+        eyes.forEach(function(eye) {
+          var point = {x: eye.arguments[0], y: eye.arguments[1]},
+            rectangle = {x: area.arguments[0], y: area.arguments[1], width: area.arguments[2], height: area.arguments[3]};
+          expect(isPointInsideRectangle(point, rectangle)).toBe(true);
+        });
+    });
+
+    ['left', 'right'].forEach(function (side) {
+      it('should have 1 eye when looked from a ' + side, function () {
+          face.draw({side: side});
+
+          var eyes = arcs(ctx.stack());
+
+          expect(eyes.length).toBe(1);
+      });
     });
 
     function circles(stack) {
@@ -32,5 +65,60 @@ describe('Face', function () {
         var angleOfFullCircle = +(2 * Math.PI).toFixed(3);
         return element.method === 'arc' && (element.arguments[4] - element.arguments[3] === angleOfFullCircle);
       })
+    }
+
+    function arcs(stack) {
+      return stack.filter(function(element) {
+        var angleOfFullCircle = +(2 * Math.PI).toFixed(3);
+        return element.method === 'arc' && (element.arguments[4] - element.arguments[3] < angleOfFullCircle);
+      })
+    }
+
+    function rectangles(stack) {
+      return stack.filter(function(element) {
+        return element.method === 'strokeRect';
+      })
+    }
+
+    function radius(circle) {
+      return circle.arguments[2];
+    }
+
+    function position(circle) {
+      return {x: circle.arguments[0], y: circle.arguments[1]};
+    }
+
+    // http://stackoverflow.com/questions/2752725/finding-whether-a-point-lies-inside-a-rectangle-or-not
+    function isPointInsideRectangle(point, rectangle) {
+      var segments = [{
+        x1: rectangle.x,
+        y1: rectangle.y,
+        x2: rectangle.x + rectangle.width,
+        y2: rectangle.y }, {
+        x1: rectangle.x + rectangle.width,
+        y1: rectangle.y,
+        x2: rectangle.x + rectangle.width,
+        y2: rectangle.y + rectangle.height}, {
+        x1: rectangle.x + rectangle.width,
+        y1: rectangle.y + rectangle.height,
+        x2: rectangle.x,
+        y2: rectangle.y + rectangle.height}, {
+        x1: rectangle.x,
+        y1: rectangle.y + rectangle.height,
+        x2: rectangle.x,
+        y2: rectangle.y
+        }];
+
+      var isInside = segments.map(function(segment) {
+        var A = -(segment.y2 - segment.y1),
+          B = segment.x2 - segment.x1,
+          C = -(A * segment.x1 + B * segment.y1),
+          D = A * point.x + B * point.y + C;
+          return D;
+      }).some(function(D) {
+        return D > 0;
+      });
+
+      return isInside;
     }
 });
