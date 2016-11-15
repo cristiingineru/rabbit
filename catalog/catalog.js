690c987a2540c6b14df366cb7a4b86f31d258d50
Cristi Ingineru <cristi.ingineru@gmail.com>
2016-11-15 22:31:19 +0200

    Live preview

--- catalog/catalog.js
+++ catalog/catalog.js
@@ -1,132 +1,141 @@
 "use strict";
 
-var examples = [
+var rabbit,
+  examples = [
 
-  function(ctx, sizeIndex) {
-    ctx.beginPath();
-    ctx.lineWidth = sizeIndex*2 + 1;
-    ctx.scale(sizeIndex+1, sizeIndex+1);
-    ctx.moveTo(10.5, 10.5);
-    ctx.lineTo(30.5, 30.5);
-    ctx.moveTo(35.5,  5.5);
-    ctx.lineTo(15.5, 25.5);
-    ctx.stroke();
-  },
+`//inputs: ctx, sizeIndex;
+ctx.beginPath();
+ctx.lineWidth = sizeIndex*2 + 1;
+ctx.scale(sizeIndex+1, sizeIndex+1);
+ctx.moveTo(10.5, 10.5);
+ctx.lineTo(30.5, 30.5);
+ctx.moveTo(35.5,  5.5);
+ctx.lineTo(15.5, 25.5);
+ctx.stroke();
+`,
 
-  function(ctx, sizeIndex) {
-    ctx.scale(sizeIndex+1, sizeIndex+1);
-    ctx.beginPath();
-    ctx.moveTo(25.5,  5.5);
-    ctx.lineTo(25.5, 45.5);
-    ctx.lineWidth = 20;
-    ctx.strokeStyle = 'black';
-    ctx.stroke();
-    ctx.lineWidth = 15;
-    ctx.strokeStyle = 'yellow';
-    ctx.stroke();
-    ctx.lineWidth = 10;
-    ctx.strokeStyle = 'purple';
-    ctx.stroke();
-  },
+`//inputs: ctx, sizeIndex;
+ctx.scale(sizeIndex+1, sizeIndex+1);
+ctx.beginPath();
+ctx.moveTo(25.5,  5.5);
+ctx.lineTo(25.5, 45.5);
+ctx.lineWidth = 20;
+ctx.strokeStyle = 'black';
+ctx.stroke();
+ctx.lineWidth = 15;
+ctx.strokeStyle = 'yellow';
+ctx.stroke();
+ctx.lineWidth = 10;
+ctx.strokeStyle = 'purple';
+ctx.stroke();
+`,
 
-  function(ctx, sizeIndex) {
-    ctx.scale(sizeIndex*1.1+0.5, sizeIndex*1.1+0.5);
-    ctx.beginPath();
-    ctx.lineWidth = 4;
-    ctx.moveTo( 5.5, 10.5);
-    ctx.lineTo( 5.5, 20.5);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.lineWidth = 5;
-    ctx.moveTo(10.5, 10.5);
-    ctx.lineTo(20.5, 10.5);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.lineWidth = 8;
-    ctx.moveTo(25.5, 10.5);
-    ctx.lineTo(35.5, 10.5);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.lineWidth = 15;
-    ctx.moveTo(45.5, 20.5);
-    ctx.lineTo(30.5, 45.5);
-    ctx.stroke();
-  },
+`//inputs: ctx, sizeIndex;
+ctx.scale(sizeIndex*1.1+0.5, sizeIndex*1.1+0.5);
+ctx.beginPath();
+ctx.lineWidth = 4;
+ctx.moveTo( 5.5, 10.5);
+ctx.lineTo( 5.5, 20.5);
+ctx.stroke();
+ctx.beginPath();
+ctx.lineWidth = 5;
+ctx.moveTo(10.5, 10.5);
+ctx.lineTo(20.5, 10.5);
+ctx.stroke();
+ctx.beginPath();
+ctx.lineWidth = 8;
+ctx.moveTo(25.5, 10.5);
+ctx.lineTo(35.5, 10.5);
+ctx.stroke();
+ctx.beginPath();
+ctx.lineWidth = 15;
+ctx.moveTo(45.5, 20.5);
+ctx.lineTo(30.5, 45.5);
+ctx.stroke();
+`,
 
-  function(ctx, sizeIndex) {
-    ctx.beginPath();
-    ctx.lineWidth = sizeIndex * 2 + 1;
-    ctx.moveTo(10.5, 10.5);
-    ctx.lineTo(30.5, 30.5);
-    ctx.lineTo(30.5, 10.5);
-    ctx.lineTo(10.5, 30.5);
-    ctx.stroke();
-  },
+`//inputs: ctx, sizeIndex;
+ctx.beginPath();
+ctx.lineWidth = sizeIndex * 2 + 1;
+ctx.moveTo(10.5, 10.5);
+ctx.lineTo(30.5, 30.5);
+ctx.lineTo(30.5, 10.5);
+ctx.lineTo(10.5, 30.5);
+ctx.stroke();
+`,
 
-  function(ctx, sizeIndex) {
-    ctx.beginPath();
-    ctx.lineWidth = sizeIndex * 2 + 1;
-    ctx.strokeRect(10.5, 10.5, 15, 15);
-    ctx.strokeRect(30.5, 30.5,  5,  5);
-    ctx.strokeRect(35.5,  5.5,  8,  8);
-  },
+`//inputs: ctx, sizeIndex;
+ctx.beginPath();
+ctx.lineWidth = sizeIndex * 2 + 1;
+ctx.strokeRect(10.5, 10.5, 15, 15);
+ctx.strokeRect(30.5, 30.5,  5,  5);
+ctx.strokeRect(35.5,  5.5,  8,  8);
+`,
 
-  function(ctx, sizeIndex) {
-    ctx.beginPath();
-    ctx.lineWidth = sizeIndex * 2 + 1;
-    ctx.strokeRect(5.5, 5.5, 15, 15);
-    ctx.translate(5, 5);
-    ctx.strokeRect(5.5, 5.5, 15, 15);
-    ctx.translate(5, 5);
-    ctx.strokeRect(5.5, 5.5, 15, 15);
-  },
+`//inputs: ctx, sizeIndex;
+ctx.beginPath();
+ctx.lineWidth = sizeIndex * 2 + 1;
+ctx.strokeRect(5.5, 5.5, 15, 15);
+ctx.translate(5, 5);
+ctx.strokeRect(5.5, 5.5, 15, 15);
+ctx.translate(5, 5);
+ctx.strokeRect(5.5, 5.5, 15, 15);
+`,
 
-  function(ctx, sizeIndex) {
-    ctx.beginPath();
-    ctx.lineWidth = sizeIndex * 2 + 1;
-    ctx.scale(sizeIndex + 1, sizeIndex + 1);
-    ctx.arc(20, 20, 10, 2*Math.PI/4, 6*Math.PI/4);
-    ctx.stroke();
-  },
+`//inputs: ctx, sizeIndex;
+ctx.beginPath();
+ctx.lineWidth = sizeIndex * 2 + 1;
+ctx.scale(sizeIndex + 1, sizeIndex + 1);
+ctx.arc(20, 20, 10, 2*Math.PI/4, 6*Math.PI/4);
+ctx.stroke();
+`,
 
-  function(ctx, sizeIndex) {
-    ctx.scale(sizeIndex*0.3 + 1, sizeIndex*0.3 + 1);
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    ctx.translate(0, 0);
-    ctx.arc(15, 15, 10, 7*Math.PI/8, 13*Math.PI/8);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.lineWidth = 3;
-    ctx.translate(5, 5);
-    ctx.scale(1.2, 1.2);
-    ctx.arc(15, 15, 10, 7*Math.PI/8, 13*Math.PI/8);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.lineWidth = 5;
-    ctx.translate(5, 5);
-    ctx.scale(1.2, 1.2);
-    ctx.arc(15, 15, 8, 0*Math.PI/4, 16*Math.PI/8);
-    ctx.stroke();
-  },
+`//inputs: ctx, sizeIndex;
+ctx.beginPath();
+ctx.lineWidth = sizeIndex * 2 + 1;
+ctx.scale(sizeIndex + 1, sizeIndex + 1);
+ctx.arc(20, 20, 10, 2*Math.PI/4, 6*Math.PI/4);
+ctx.stroke();
+`,
 
-  function(ctx, sizeIndex) {
-    var rectWidth = 30;
-    var rectHeight = 20;
-    var rectX = 5;
-    var rectY = 5;
-    var cornerRadius = 7;
-    ctx.scale(1+sizeIndex*0.2, 1+sizeIndex*0.2);
-    ctx.beginPath();
-    ctx.moveTo(rectX, rectY);
-    ctx.lineTo(rectX+rectWidth-cornerRadius, rectY);
-    ctx.arcTo(rectX+rectWidth, rectY,
-      rectX+rectWidth, rectY+cornerRadius,
-      cornerRadius);
-    ctx.lineTo(rectX+rectWidth, rectY+rectHeight);
-    ctx.lineWidth = 3 * sizeIndex + 1;
-    ctx.stroke();
-  },
+`//inputs: ctx, sizeIndex;
+ctx.scale(sizeIndex*0.3 + 1, sizeIndex*0.3 + 1);
+ctx.beginPath();
+ctx.lineWidth = 1;
+ctx.translate(0, 0);
+ctx.arc(15, 15, 10, 7*Math.PI/8, 13*Math.PI/8);
+ctx.stroke();
+ctx.beginPath();
+ctx.lineWidth = 3;
+ctx.translate(5, 5);
+ctx.scale(1.2, 1.2);
+ctx.arc(15, 15, 10, 7*Math.PI/8, 13*Math.PI/8);
+ctx.stroke();
+ctx.beginPath();
+ctx.lineWidth = 5;
+ctx.translate(5, 5);
+ctx.scale(1.2, 1.2);
+ctx.arc(15, 15, 8, 0*Math.PI/4, 16*Math.PI/8);
+ctx.stroke();
+`,
+
+`//inputs: ctx, sizeIndex;
+var rectWidth = 30;
+var rectHeight = 20;
+var rectX = 5;
+var rectY = 5;
+var cornerRadius = 7;
+ctx.scale(1+sizeIndex*0.2, 1+sizeIndex*0.2);
+ctx.beginPath();
+ctx.moveTo(rectX, rectY);
+ctx.lineTo(rectX+rectWidth-cornerRadius, rectY);
+ctx.arcTo(rectX+rectWidth, rectY,
+  rectX+rectWidth, rectY+cornerRadius,
+  cornerRadius);
+ctx.lineTo(rectX+rectWidth, rectY+rectHeight);
+ctx.lineWidth = 3 * sizeIndex + 1;
+ctx.stroke();
+`
 
 ];
 
@@ -139,6 +148,44 @@ function drawBBox(ctx, rabbit) {
   ctx.strokeRect(box.x, box.y, box.width, box.height);
 }
 
+function createTextAreasWithExamples()
+{
+  examples.forEach(function(example, exampleIndex) {
+    var examplePreviews = $('#placeholder')
+      .append('<textarea type="text" cols="55" rows="10" class="code"/>')
+      .find('textarea'),
+      examplePreview = examplePreviews[examplePreviews.length - 1];
+    examplePreview.value = example.toString();
+  });
+}
+
+function runExamples() {
+  $('.demo').remove();
+  $('.demoBreak').remove();
+  $('.code').each(function(exampleIndex, textArea) {
+
+    var textArea = $(textArea),
+      lastSibling = textArea;
+
+    [{width:  50, height:  50},
+     {width: 100, height: 100},
+     {width: 150, height: 150}
+    ].forEach(function(size, sizeIndex) {
+
+      lastSibling.after('<canvas class="demo" width="' + size.width + '" height="' + size.height + '" style="margin: 5px;" />');
+      var canvases = textArea.parent().find('canvas'),
+        lastCreatedCanvas = canvases[canvases.length - 1],
+        ctx = lastCreatedCanvas.getContext('2d');
+      lastSibling = $(lastCreatedCanvas);
+
+      eval(textArea.val());
+      drawBBox(ctx, rabbit);
+    });
+
+    lastSibling.after('<br class="demoBreak" />');
+  });
+}
+
 
 requirejs.config({
   baseUrl: '../build/amd',
@@ -149,34 +196,10 @@ requirejs.config({
 
 
 requirejs(['lib/domReady', 'lib/canteen.min', '../build/amd/rabbit.js'], function(domReady, Canteen, RabbitModule) {
-  
-  var rabbit = new RabbitModule.Rabbit();
 
-  examples.forEach(function(example, exampleIndex) {
+  rabbit = new RabbitModule.Rabbit();
 
-    var examplePreviews = $('#placeholder')
-      .append('<textarea type="text" cols="55" rows="10" />')
-      .find('textarea'),
-      examplePreview = examplePreviews[examplePreviews.length - 1];
-    examplePreview.value = example.toString();
-
-    [{width:  50, height:  50},
-     {width: 100, height: 100},
-     {width: 150, height: 150}
-    ].forEach(function(size, sizeIndex) {
-      
-      var canvases = $('#placeholder')
-        .append('<canvas width="' + size.width + '" height="' + size.height + '" style="margin: 5px;" />')
-        .find('canvas'),
-      lastCreatedCanvas = canvases[canvases.length - 1],
-      ctx = lastCreatedCanvas.getContext('2d');
-
-      example(ctx, sizeIndex);
-      drawBBox(ctx, rabbit);
-    });
-
-    $('#placeholder').append('<br />');
-  });
-
+  createTextAreasWithExamples();
+  runExamples();
 
 });
