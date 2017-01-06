448cbdffba7ee8c6fdf44e967773b4e2bd5fdbf0
Cristi Ingineru <cristi.ingineru@gmail.com>
2017-01-06 11:48:11 +0200

    Fix the phantomjs tests

--- karma.conf.js
+++ karma.conf.js
@@ -14,6 +14,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'node_modules/phantomjs-polyfill-object-assign/object-assign-polyfill.js',
+      'node_modules/phantomjs-polyfill-find/find-polyfill.js',
 
       // This has to be loaded with requirejs
       {pattern: 'node_modules/Canteen/build/canteen.min.js', included: false, served: true, watched: true, nocache: true},
